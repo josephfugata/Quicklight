ca7b33a6f957f72d4d2267175a106e383bcbcaa2
Joseph Fugata <workwithjosephfugata@gmail.com>
2025-09-28 06:22:26 +0000

    When viewing the entire site on large screen why is the site secctions s

--- src/components/landing/financing.tsx
+++ src/components/landing/financing.tsx
@@ -10,7 +10,7 @@ export default function Financing() {
   return (
     <section id="financing" className="w-full py-12 md:py-24 lg:py-32">
       <div className="container px-4 md:px-6">
-        <div className="grid gap-10 lg:grid-cols-2 lg:gap-16">
+        <div className="grid gap-10 lg:grid-cols-2 lg:gap-16 mx-auto max-w-5xl">
           <div className="space-y-4">
             <Badge variant="outline">Financial Incentives</Badge>
             <h2 className="font-headline text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
--- src/components/landing/our-technology.tsx
+++ src/components/landing/our-technology.tsx
@@ -26,7 +26,7 @@ export default function OurTechnology() {
   return (
     <section id="technology" className="w-full py-12 md:py-24 lg:py-32">
       <div className="container px-4 md:px-6">
-        <div className="grid gap-10 lg:grid-cols-2 lg:gap-16">
+        <div className="grid gap-10 lg:grid-cols-2 lg:gap-16 mx-auto max-w-5xl">
           <div className="flex flex-col justify-center space-y-4">
             <Badge variant="outline">Our Technology</Badge>
             <h2 className="font-headline text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
