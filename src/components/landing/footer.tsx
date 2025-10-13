71df29fe47219d55a054fd9c13a15c0a4c29d9d5
Joseph Fugata <workwithjosephfugata@gmail.com>
2025-10-13 10:23:05 +0000

    the footer is still full width on desktop, please don't

--- src/components/landing/footer.tsx
+++ src/components/landing/footer.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Footer() {
   return (
     <footer className="w-full bg-background border-t">
-      <div className="container flex h-14 max-w-screen-2xl flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
+      <div className="container mx-auto flex h-14 max-w-screen-2xl flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <Image src="/Logo.png" alt="QuickLight Logo" width={32} height={32} className="h-8 w-8" />
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
